2e8cccae9cba641f9bc40b661bf0dd35b800bb85
student <student@GBTC440J10LX.montgomerycollege.edu>
2017-05-24 13:45:38 -0400

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+
+/*
+ * This is a multiline comment
+ * Author: Raya
+ * Date: 5/24/2017
+ * This is my first java program
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		//This is the main method
+		
+	System.out.println("Hello WOrld!");
+
+	}
+
+}
